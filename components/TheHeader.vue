e259047d85c2531cb0cb6c29a7da4395420527da
Jesus <jlazar135@gmail.com>
2023-08-22 13:55:37 +0200

    se incluye badge y carrito en header

---
+++ components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="shadow-sm py-4">
+    <UContainer>
+      <nav class="flex justify-between items-center">
+        <NuxtLink to="/">
+            <div class="w-[100px]">
+                <img src="@/i_logo.png" class="w-full h-auto object-cover" alt="logo">
+            </div>
+        </NuxtLink>
+        <ul class="flex gap-4 items-center">
+            <li><NuxtLink to="/about">Conéctate</NuxtLink></li>
+            <li><NuxtLink to="/products">
+                <UButton icon="i-heroicons-shopping-cart" size="xl" color="primary" variant="ghost" :trailing="false" class="relative">
+                    <UBadge 
+                    size="xs" 
+                    :ui="{ rounded: 'rounded-full' }" 
+                    class="min-w-[20px] aspect-square flex items-center justify-center absolute top-0 right-0">
+                        1
+                    </UBadge>
+                </UButton>    
+            </NuxtLink></li>
+        </ul>
+    </nav>
+    </UContainer>
+  </header>
+</template> 
+
+<style scoped>
+.router-link-exact-active {
+  color: rgb(44, 152, 1);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,5 @@
 <template>
-  <header class="shadow-sm py-4">
-    <UContainer>
-      <nav class="flex justify-between">
-        <NuxtLink to="/">LOGO</NuxtLink>
-        <ul class="flex gap-4">
-            <li><NuxtLink to="/">Inicio</NuxtLink></li>
-            <li><NuxtLink to="/products">Productos</NuxtLink></li>
-            <li><NuxtLink to="/about">Nosotros</NuxtLink></li>
-        </ul>
-    </nav>
-    </UContainer>
-  </header>
+  <TheHeader></TheHeader>
   <div class="container mx-auto p-4 ">
       <slot />
   </div>
@@ -18,8 +7,4 @@
 
 <script setup></script>
 
-<style scoped>
-.router-link-exact-active {
-  color: rgb(44, 152, 1);
-}
-</style>
+<style scoped></style>
